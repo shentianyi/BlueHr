f61597982fb5a80bbe9f080e8a146a5949a63dbe
SongFuZhen <evil_song@126.com>
2016-08-26 15:03:19 +0800

    layout

---
+++ code/BlueHr/BlueHrWeb/Scripts/layout.js
@@ -0,0 +1,17 @@
+﻿var Layout = {};
+
+Layout.init = function () {
+    $('.nav-left dt').removeClass('active');
+
+    var pathname = window.location.pathname.split('/')[1];
+
+    console.log(pathname);
+
+    switch (pathname) {
+        case "Company":
+            $('.nav-company').addClass('active');
+            break;
+        default:
+            break;
+    }
+}
