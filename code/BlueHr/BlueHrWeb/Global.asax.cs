dbc6cd41c5711149ea9b2fe191b3cef935bb7270
tonyy <yangshengtao_cool@126.com>
2016-08-30 21:57:11 +0800

    增加常用公共组件

--- code/BlueHr/BlueHrWeb/Global.asax.cs
+++ code/BlueHr/BlueHrWeb/Global.asax.cs
@@ -6,18 +6,28 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BlueHrLib.Log;
 
 namespace BlueHrWeb
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        ILogger logger = LogFactory.CreateLogger(typeof(MvcApplication));
+
         protected void Application_Start()
         {
+            logger.Info("系统启动...");
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_End()
+        {
+            logger.Info("系统停止");
         }
     }
 }
