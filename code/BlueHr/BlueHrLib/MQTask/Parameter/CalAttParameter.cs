18c520e2e25dc34c0ec0d330c92307e8cf7baca6
Charlot <7wangsong@gmail.com>
2016-09-05 19:34:53 +0800

    change attcal name

---
+++ code/BlueHr/BlueHrLib/MQTask/Parameter/CalAttParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlueHrLib.MQTask.Parameter
+{
+    [Serializable]
+    public class CalAttParameter
+    {
+        /// <summary>
+        /// 计算时间
+        /// </summary>
+        public DateTime AttDateTime { get; set; }
+
+        /// <summary>
+        /// 班次列表
+        /// </summary>
+        public List<string> ShiftCodes { get; set; }
+    }
+}
