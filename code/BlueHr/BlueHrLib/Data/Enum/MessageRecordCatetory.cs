096bae640909bc45d685105a7a8e5ac6cdb4469e
Charlot <7wangsong@gmail.com>
2016-09-12 18:48:06 +0800

    on message record list

---
+++ code/BlueHr/BlueHrLib/Data/Enum/MessageRecordCatetory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlueHrLib.Data.Enum
+{
+    public enum MessageRecordCatetory
+    {
+        EditStaff=100,
+        ManageStaff=200,
+        Alert=300
+    }
+}
--- code/BlueHr/BlueHrWeb/Controllers/MessageRecordController.cs
+++ code/BlueHr/BlueHrWeb/Controllers/MessageRecordController.cs
@@ -1,5 +1,7 @@
 ﻿using BlueHrLib.Data;
+using BlueHrLib.Data.Enum;
 using BlueHrLib.Data.Message;
+using BlueHrLib.Data.Model.Search;
 using BlueHrLib.Service.Implement;
 using BlueHrLib.Service.Interface;
 using BlueHrWeb.Helpers;
@@ -19,25 +21,44 @@ namespace BlueHrWeb.Controllers
         // GET: MessageRecord
         // allOrUnread: unread/all
         // type: alert/manage/basic/all
-        public ActionResult Index(string allOrUnread="unread",string type="alert",int? page=null)
+        public ActionResult Index(string allOrUnread="unread",int? page=null)
         {
-            ViewBag.allorUnread = allOrUnread;
-            ViewBag.type = type;
+            ViewBag.allOrUnread = allOrUnread;
 
+            MessageRecordSearchModel q = new MessageRecordSearchModel();
 
             int pageIndex = PagingHelper.GetPageIndex(page);
             IMessageRecordService mrs = new MessageRecordService(Settings.Default.db);
             IPagedList<MessageRecordView> records = mrs
-                .GetByTypesAndAllOrUnread(MessageCategory.CateListDic[type], (allOrUnread=="all" ? true : false))
-                .ToPagedList<MessageRecordView>(pageIndex, Settings.Default.pageSize);
+                .GetByCateAndAllOrUnread(MessageRecordCatetory.Alert, (allOrUnread=="all" ? true : false))
+                .ToPagedList<MessageRecordView>(pageIndex, 2);
 
+            ViewBag.Query = q;
 
-            ViewBag.alertCount = 0;
-            ViewBag.manageCount = 0;
-            ViewBag.basicCount = 0;
-            ViewBag.all = 0;
             return View(records);
         }
+
+
+        public ActionResult Search([Bind(Include = "StaffNr,StaffNrAct")] MessageRecordSearchModel q)
+        {
+            string allOrUnread = Request.QueryString["allOrUnread"];
+            ViewBag.allOrUnread = allOrUnread;
+          
+
+            int pageIndex = 0;
+            int.TryParse(Request.QueryString.Get("page"), out pageIndex);
+            pageIndex = PagingHelper.GetPageIndex(pageIndex);
+
+            IMessageRecordService mrs = new MessageRecordService(Settings.Default.db);
+            IPagedList<MessageRecordView> records = mrs
+                .GetByCateAndAllOrUnread(MessageRecordCatetory.Alert, (allOrUnread == "all" ? true : false),q)
+                .ToPagedList<MessageRecordView>(pageIndex, 2);
+
+            ViewBag.Query = q;
+
+            return View("Index", records);
+        }
+
 
         [HttpGet] 
         public ActionResult UnReadCount()
