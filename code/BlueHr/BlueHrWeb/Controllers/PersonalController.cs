01cab80c270ee73f97147dbf7f61e81c1b84c10b
SongFuZhen <evil_song@126.com>
2016-12-06 16:16:59 +0800

    application

--- code/BlueHr/BlueHrWeb/Controllers/PersonalController.cs
+++ code/BlueHr/BlueHrWeb/Controllers/PersonalController.cs
@@ -49,7 +49,8 @@ namespace BlueHrWeb.Controllers
                 extraWorks.Add("Duration", extraWork.duration + " (小时)");
                 //extraWorks.Add("DurationType", extraWork.durationType==100?"天":"小时");
                 extraWorks.Add("Reason", extraWork.otReason);
-                extraWorks.Add("ApprovalStatus", extraWork.approvalStatus == null ? "审核中" : extraWork.approvalStatus);
+                extraWorks.Add("ApprovalStatus", extraWork.approvalStatus == null ? "审批中" : extraWork.approvalStatus);
+                extraWorks.Add("ApprovalRemarks", extraWork.remarks);
 
                 AllExtraWork.Add(extraWorks);
             }
