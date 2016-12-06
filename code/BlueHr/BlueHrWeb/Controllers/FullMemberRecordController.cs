00c7d9e4bdacc1d364ba1f01c5ff104118ad7e12
Propesiya <287157438@qq.com>
2016-12-06 16:11:43 +0800

    fix

--- code/BlueHr/BlueHrWeb/Controllers/FullMemberRecordController.cs
+++ code/BlueHr/BlueHrWeb/Controllers/FullMemberRecordController.cs
@@ -30,18 +30,18 @@ namespace BlueHrWeb.Controllers
 
             IFullMemberRecordService raps = new FullMemberRecordService(Settings.Default.db);
 
-            IPagedList<FullMemberRecord> rewardsAndPenalties = raps.Search(q).ToPagedList(pageIndex, Settings.Default.pageSize);
+            IPagedList<FullMemberRecord> fullMemberRecord = raps.Search(q).ToPagedList(pageIndex, Settings.Default.pageSize);
 
             ViewBag.Query = q;
 
             FullMemberRecordInfoModel info = raps.GetFullMemberRecordInfo(q);
             ViewBag.Info = info;
 
-            return View(rewardsAndPenalties);
+            return View(fullMemberRecord);
         }
 
         [RoleAndDataAuthorizationAttribute]
-        public ActionResult Search([Bind(Include = "Name")] FullMemberRecordSearchModel q)
+        public ActionResult Search([Bind(Include = "staffNr")] FullMemberRecordSearchModel q)
         {
             int pageIndex = 0;
             int.TryParse(Request.QueryString.Get("page"), out pageIndex);
@@ -49,11 +49,11 @@ namespace BlueHrWeb.Controllers
 
             IFullMemberRecordService raps = new FullMemberRecordService(Settings.Default.db);
 
-            IPagedList<FullMemberRecord> rewardsAndPenalties = raps.Search(q).ToPagedList(pageIndex, Settings.Default.pageSize);
+            IPagedList<FullMemberRecord> fullMemberRecord = raps.Search(q).ToPagedList(pageIndex, Settings.Default.pageSize);
 
             ViewBag.Query = q;
 
-            return View("Index", rewardsAndPenalties);
+            return View("Index", fullMemberRecord);
         }
 
 
@@ -75,13 +75,13 @@ namespace BlueHrWeb.Controllers
         // POST: FullMemberRecord/Create
         [RoleAndDataAuthorizationAttribute]
         [HttpPost]
-        public JsonResult Create([Bind(Include = "staffNr,staffName,staffSex,companyId,departmentId,type,project,description,createAt,userId")] FullMemberRecord rewardsAndPenalties)
+        public JsonResult Create([Bind(Include = "staffNr,isPassCheck,checkScore,beFullAt,remark,checkAt,beFullChecker,approvalAt,approvalStatus,approvalRemark,createdAt,userId")] FullMemberRecord fullMemberRecord)
         {
             ResultMessage msg = new ResultMessage();
 
             try
             {
-                msg = DoValidation(rewardsAndPenalties);
+                msg = DoValidation(fullMemberRecord);
 
                 if (!msg.Success)
                 {
@@ -90,7 +90,7 @@ namespace BlueHrWeb.Controllers
                 else
                 {
                     IFullMemberRecordService raps = new FullMemberRecordService(Settings.Default.db);
-                    bool isSucceed = raps.Create(rewardsAndPenalties);
+                    bool isSucceed = raps.Create(fullMemberRecord);
                     msg.Success = isSucceed;
                     msg.Content = isSucceed ? "" : "添加失败";
 
@@ -109,45 +109,24 @@ namespace BlueHrWeb.Controllers
         {
             IFullMemberRecordService cs = new FullMemberRecordService(Settings.Default.db);
 
-            FullMemberRecord rewardsAndPenalties = cs.FindById(id);
+            FullMemberRecord fullMemberRecord = cs.FindById(id);
 
-            //SetDropDownList(rewardsAndPenalties);
+            //SetDropDownList(fullMemberRecord);
 
-            return View(rewardsAndPenalties);
+            return View(fullMemberRecord);
         }
 
-        //[HttpGet]
-        //public JsonResult detail(string staffNr)
-        //{
-        //    IFullMemberRecordService rps = new FullMemberRecordService(Settings.Default.db);
-        //    List<Dictionary<string, string>> Result = new List<Dictionary<string, string>>();
-        //    foreach (var i in rps.FindBystaffNr(staffNr))
-        //    {
-        //        Dictionary<string, string> detail = new Dictionary<string, string>();
-        //        detail.Add("奖惩类型", i.type == 1 ? "奖励" : "惩罚");
-        //        detail.Add("奖惩项目", i.project);
-        //        detail.Add("奖惩描述", i.description);
-        //        detail.Add("奖惩日期", i.createAt.ToString());
-        //        IUserService us = new UserService(Settings.Default.db);
-        //        try
-        //        {
-        //            detail.Add("审批人", us.FindById((int)i.userId).name);
-        //        }
-        //        catch { detail.Add("审批人", null); }
-        //        Result.Add(detail);
-        //    }
-        //    return Json(Result, JsonRequestBehavior.AllowGet);
-        //}
+        
         // POST: FullMemberRecord/Edit/5
         [RoleAndDataAuthorizationAttribute]
         [HttpPost]
-        public JsonResult Edit([Bind(Include = "nr,name,sex,companyId,departmentId,type,project,description,datetime,approver")] FullMemberRecord rewardsAndPenalties)
+        public JsonResult Edit([Bind(Include = "staffNr,isPassCheck,checkScore,beFullAt,remark,checkAt,beFullChecker,approvalAt,approvalStatus,approvalRemark,createdAt,userId")] FullMemberRecord fullMemberRecord)
         {
             ResultMessage msg = new ResultMessage();
 
             try
             {
-                msg = DoValidation(rewardsAndPenalties);
+                msg = DoValidation(fullMemberRecord);
 
                 if (!msg.Success)
                 {
@@ -156,7 +135,7 @@ namespace BlueHrWeb.Controllers
                 else
                 {
                     IFullMemberRecordService raps = new FullMemberRecordService(Settings.Default.db);
-                    bool isSucceed = raps.Update(rewardsAndPenalties);
+                    bool isSucceed = raps.Update(fullMemberRecord);
 
                     msg.Success = isSucceed;
                     msg.Content = isSucceed ? "" : "更新失败";
@@ -176,9 +155,9 @@ namespace BlueHrWeb.Controllers
         {
             IFullMemberRecordService raps = new FullMemberRecordService(Settings.Default.db);
 
-            FullMemberRecord rewardsAndPenalties = raps.FindById(id);
-            //SetDropDownList(rewardsAndPenalties);
-            return View(rewardsAndPenalties);
+            FullMemberRecord fullMemberRecord = raps.FindById(id);
+            //SetDropDownList(fullMemberRecord);
+            return View(fullMemberRecord);
         }
 
         // POST: FullMemberRecord/Delete/5
@@ -189,8 +168,8 @@ namespace BlueHrWeb.Controllers
             ResultMessage msg = new ResultMessage();
             try
             {
-                IFullMemberRecordService raps = new FullMemberRecordService(Settings.Default.db);
-                bool isSucceed = raps.DeleteById(id);
+                IFullMemberRecordService mrs = new FullMemberRecordService(Settings.Default.db);
+                bool isSucceed = mrs.DeleteById(id);
                 msg.Success = isSucceed;
                 msg.Content = isSucceed ? "" : "删除失败";
                 return Json(msg, JsonRequestBehavior.AllowGet);
@@ -201,131 +180,6 @@ namespace BlueHrWeb.Controllers
             }
         }
 
-        //private void SetDropDownList(FullMemberRecord fullMemberRecord)
-        //{
-        //    if (fullMemberRecord != null)
-        //    {
-        //        SetSexList(fullMemberRecord.staffSex);
-        //        SetCompanyList(fullMemberRecord.companyId);
-        //        SetDepartmentList(fullMemberRecord.companyId, fullMemberRecord.departmentId);
-        //        SetTypeList(fullMemberRecord.type);
-        //    }
-        //    else
-        //    {
-        //        SetSexList(null);
-        //        SetCompanyList(null);
-        //        SetDepartmentList(null, null);
-        //        SetTypeList(null);
-        //    }
-        //}
-        //private void SetSexList(int? type, bool allowBlank = true)
-        //{
-        //    List<EnumItem> item = EnumHelper.GetList(typeof(Sex));
-
-        //    List<SelectListItem> select = new List<SelectListItem>();
-
-        //    if (allowBlank)
-        //    {
-        //        select.Add(new SelectListItem { Text = "", Value = "" });
-        //    }
-
-        //    foreach (var it in item)
-        //    {
-        //        if (type.HasValue && type.ToString().Equals(it.Value))
-        //        {
-        //            select.Add(new SelectListItem { Text = it.Text, Value = it.Value.ToString(), Selected = true });
-        //        }
-        //        else
-        //        {
-        //            select.Add(new SelectListItem { Text = it.Text, Value = it.Value.ToString(), Selected = false });
-        //        }
-        //    }
-        //    ViewData["sexList"] = select;
-        //}
-        //private void SetTypeList(int? type, bool allowBlank = true)
-        //{
-        //    List<EnumItem> item = EnumHelper.GetList(typeof(FullMemberRecordType));
-
-        //    List<SelectListItem> select = new List<SelectListItem>();
-
-        //    if (allowBlank)
-        //    {
-        //        select.Add(new SelectListItem { Text = "", Value = "" });
-        //    }
-
-        //    foreach (var it in item)
-        //    {
-        //        if (type.HasValue && type.ToString().Equals(it.Value))
-        //        {
-        //            select.Add(new SelectListItem { Text = it.Text, Value = it.Value.ToString(), Selected = true });
-        //        }
-        //        else
-        //        {
-        //            select.Add(new SelectListItem { Text = it.Text, Value = it.Value.ToString(), Selected = false });
-        //        }
-        //    }
-        //    ViewData["rewardsAndPenaltiesTypeList"] = select;
-        //}
-        //private void SetDepartmentList(int? companyId, int? type, bool allowBlank = true)
-        //{
-        //    IDepartmentService ds = new DepartmentService(Settings.Default.db);
-
-        //    List<SelectListItem> select = new List<SelectListItem>();
-
-        //    List<Department> departments = new List<Department>();
-        //    if (companyId.HasValue)
-        //    {
-        //        departments = ds.FindByCompanyId(companyId).ToList();
-
-        //        if (allowBlank)
-        //        {
-        //            select.Add(new SelectListItem { Text = "", Value = "" });
-        //        }
-
-        //        foreach (var department in departments)
-        //        {
-        //            if (type.HasValue && type.ToString().Equals(department.id))
-        //            {
-        //                select.Add(new SelectListItem { Text = department.fullName, Value = department.id.ToString(), Selected = true });
-        //            }
-        //            else
-        //            {
-        //                select.Add(new SelectListItem { Text = department.fullName, Value = department.id.ToString(), Selected = false });
-        //            }
-        //        }
-        //    }
-
-        //    ViewData["departmentList"] = select;
-        //}
-
-        //private void SetCompanyList(int? type, bool allowBlank = true)
-        //{
-        //    ICompanyService cs = new CompanyService(Settings.Default.db);
-
-        //    CompanySearchModel csm = new CompanySearchModel();
-
-        //    List<Company> companies = cs.Search(csm).ToList();
-
-        //    List<SelectListItem> select = new List<SelectListItem>();
-
-        //    if (allowBlank)
-        //    {
-        //        select.Add(new SelectListItem { Text = "", Value = "" });
-        //    }
-
-        //    foreach (var company in companies)
-        //    {
-        //        if (type.HasValue && type.ToString().Equals(company.id))
-        //        {
-        //            select.Add(new SelectListItem { Text = company.name, Value = company.id.ToString(), Selected = true });
-        //        }
-        //        else
-        //        {
-        //            select.Add(new SelectListItem { Text = company.name, Value = company.id.ToString(), Selected = false });
-        //        }
-        //    }
-        //    ViewData["companyList"] = select;
-        //}
         [HttpPost]
 
         public ResultMessage DoValidation(FullMemberRecord model)
