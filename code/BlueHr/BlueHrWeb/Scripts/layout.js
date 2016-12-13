0a1578eabe195daad1cb9a41934121772b9ecbcc
“ProbieZY <1107254498@qq.com>
2016-12-13 10:02:53 +0800

    temp

--- code/BlueHr/BlueHrWeb/Scripts/layout.js
+++ code/BlueHr/BlueHrWeb/Scripts/layout.js
@@ -100,7 +100,8 @@ switch (pathname[1]) {
         PageAction('#degreetype', '新建学历', '编辑学历', '学历详情', '创建', '更新', '删除');
         break;
     case "FullMemberRecord":
-        $('.nav-fullMemberRecord').addClass('active');
+        $('.nav-regular').addClass('active');
+        $('.nav-laborrelations').addClass('active');
         PageAction('#fullMemberRecord', '新建转正申请', '编辑转正申请', '转正申请详情', '创建', '更新', '删除');
         break;
     case "StaffType":
