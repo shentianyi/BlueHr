4e35deabf3ef43bc626abce94114a4024ef34ce2
chenjing3115 <2390228022@qq.com>
2016-12-14 11:51:58 +0800

    a1

--- code/BlueHr/BlueHrWeb/Scripts/layout.js
+++ code/BlueHr/BlueHrWeb/Scripts/layout.js
@@ -311,14 +311,6 @@ switch (pathname[1]) {
             $(window).resize(function(){
                 $(".content-wrapper").css({width: $(window).width(), height: $(window).height(), maxHeight: $(window).width(), paddingTop: 0, marginLeft: 0});
             })
-        }else if(pathname[2] == "changepwd"){
-        	$(".main-header").remove();
-            $(".main-sidebar").remove();
-            $(".content-wrapper").css({width: $(window).width(), height: $(window).height(), maxHeight: $(window).width(), paddingTop: 0, marginLeft: 0});
-
-            $(window).resize(function(){
-                $(".content-wrapper").css({width: $(window).width(), height: $(window).height(), maxHeight: $(window).width(), paddingTop: 0, marginLeft: 0});
-            })
         }
     }
 }
