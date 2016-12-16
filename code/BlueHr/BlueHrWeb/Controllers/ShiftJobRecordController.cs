60d392c3548886daaf349f086dee2e3b5bd219d4
Propesiya <287157438@qq.com>
2016-12-16 15:21:47 +0800

    fix bug

--- code/BlueHr/BlueHrWeb/Controllers/ShiftJobRecordController.cs
+++ code/BlueHr/BlueHrWeb/Controllers/ShiftJobRecordController.cs
@@ -394,7 +394,11 @@ namespace BlueHrWeb.Controllers
             IShiftJobRecordService lrs = new ShiftJobRecordService(Settings.Default.db);
             ShiftJobRecordSearchModel lrsm = new ShiftJobRecordSearchModel();
             var ShiftJobRecord = lrs.Search(lrsm).ToList();
-
+            if (ShiftJobRecord.Count == 0)
+            {
+                ShiftJobRecord tempShiftJobRecord = new ShiftJobRecord();
+                ShiftJobRecord.Add(tempShiftJobRecord);
+            }
             if (ShiftJobRecord != null)
             {
                 //获取当前记录的属性
