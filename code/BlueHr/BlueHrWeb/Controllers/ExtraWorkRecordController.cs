a2fe0c717a26d26aa36bde8ac425226a7143cc85
SongFuZhen <evil_song@126.com>
2016-12-19 14:27:20 +0800

    close  new window

--- code/BlueHr/BlueHrWeb/Controllers/ExtraWorkRecordController.cs
+++ code/BlueHr/BlueHrWeb/Controllers/ExtraWorkRecordController.cs
@@ -88,7 +88,7 @@ namespace BlueHrWeb.Controllers
         // POST: ExtraWorkRecord/Create
         [RoleAndDataAuthorizationAttribute]
         [HttpPost]
-        public ActionResult Create([Bind(Include = "extraWorkTypeId,staffNr,otTime,startHour,endHour,duration,durationType,otReason")] ExtraWorkRecord model)
+        public JsonResult Create([Bind(Include = "extraWorkTypeId,staffNr,otTime,startHour,endHour,duration,durationType,otReason")] ExtraWorkRecord model)
         {
             ResultMessage msg = new ResultMessage();
 
@@ -111,17 +111,16 @@ namespace BlueHrWeb.Controllers
                     bool isSucceed = cs.Create(model);
 
                     msg.Success = isSucceed;
-                    msg.Content = isSucceed ? "" : "添加失败";
+                    msg.Content = isSucceed ? "加班申请提交成功" : "加班申请提交失败， 请检查";
 
-                    return Json(msg, JsonRequestBehavior.AllowGet);
+                    return Json(msg, JsonRequestBehavior.DenyGet);
                 } 
             }
             catch (Exception ex)
             {
-                return Json(new ResultMessage() { Success = false, Content = ex.Message }, JsonRequestBehavior.AllowGet);
+                return Json(new ResultMessage() { Success = false, Content = ex.Message }, JsonRequestBehavior.DenyGet);
             }
         }
-
 
         // GET: ExtraWorkRecord/Edit/5
         [RoleAndDataAuthorizationAttribute]
